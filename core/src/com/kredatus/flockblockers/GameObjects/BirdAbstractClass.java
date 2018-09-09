541fe6cb39a01456c1335bcb79768303dd0f6c6b
Erik Kredatus <erikkre@yahoo.ca>
2018-09-08 21:51:20 -0400

    "Bird Additions"

--- core/src/com/kredatus/flockblockers/GameObjects/BirdAbstractClass.java
+++ core/src/com/kredatus/flockblockers/GameObjects/BirdAbstractClass.java
@@ -4,35 +4,48 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 import com.kredatus.flockblockers.GameWorld.GameWorld;
-
 import java.util.Random;
 
 /**
  * Created by Erik Kredatus on 9/8/2018.
+ *                  Health Speed  Gold                        Size   Amount
+ * FlockBird  =     2      M      4(400)                      S      100
+ * ThunderBird=     4      F      15(450)                     M      30
+ * GoldBird   =     25     S      100(400)                    L      4
+ * LunarBird  =     7      F      35(525)                     S      15
+ * PhoenixBird=     50     S      30++Diamond                 XL      1
+
+
+                                            Damage      Fire Rate     Penetration       Spread
+ bow, Pistol, Machinegun, Minigun, arty     2           L            M                  S
+ crossbow, sniper, cannon, gauss cannon     4           S            L                  S
+ catapult, shotgun, canister shot, missile  1           M            L                  XL
  */
 
+
+
+
+
 public abstract class BirdAbstractClass {
-    private GameWorld world;
-    private float rotation;
-    private Circle boundingCircle;
-    private static Vector2 position;
-    private static Vector2 velocity;
-    private Vector2 acceleration;
+    protected GameWorld world;
+    protected float rotation;
+    protected Circle boundingCircle;
+    protected static Vector2 position;
+    protected static Vector2 velocity;
+    protected Vector2 acceleration;
     public float gamexvelocity;
     protected int width, height, sizeVariance, camwidth, camheight;
     protected boolean isGone;
     public float  starty;
-    private boolean isAlive;
-    private Random r;
-    private OrthographicCamera cam;
+    protected boolean isAlive;
+    protected Random r;
+    protected OrthographicCamera cam;
 
     public BirdAbstractClass(int width, int height, OrthographicCamera cam, int camwidth, int camheight) {
-
-        starty= 0;
+        position.set(r.nextInt(camwidth)+cam.position.x,r.nextInt(camheight)+cam.position.y);
         isAlive=true;
         this.cam=cam;
-        this.width = width;
-        this.height = height;
+
         this.camwidth = camwidth;
         this.camheight = camheight;
         //this.sizeVariance=sizeVariance;
@@ -46,7 +59,6 @@ public abstract class BirdAbstractClass {
 
 
     public void die(float delta){
-
         velocity.y=30;
         while (position.y+height>cam.position.y-(camheight/2)){
             velocity.add(acceleration.cpy().scl(delta));
@@ -56,4 +68,24 @@ public abstract class BirdAbstractClass {
     public void dead(float delta){
 
     }
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
+
+    public Circle getBoundingCircle() {
+        return boundingCircle;
+    }
+
 }
--- core/src/com/kredatus/flockblockers/GameObjects/Birds/WaterBird.java
+++ core/src/com/kredatus/flockblockers/GameObjects/Birds/WaterBird.java
@@ -1,9 +1,22 @@
 package com.kredatus.flockblockers.GameObjects.Birds;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.kredatus.flockblockers.GameObjects.BirdAbstractClass;
+
 /**
  * Created by Erik Kredatus on 9/8/2018.
  */
 
-public class WaterBird {
+public class WaterBird extends BirdAbstractClass {
+    public WaterBird(int width, int height, OrthographicCamera cam, int camwidth, int camheight, int sizeVariance, int health){
+        super( width,  height,  cam,  camwidth, camheight);
+        this.width = width-sizeVariance+r.nextInt(sizeVariance*2);
+        this.height = height-sizeVariance+r.nextInt(sizeVariance*2);
+    }
 
+
+    @Override
+    public void update(float delta) {
+
+    }
 }
