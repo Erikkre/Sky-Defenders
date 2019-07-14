4d621874fc8c7665ccf249b9e5e2208af7a7343d
ERIK Kredatus <erik.kredatus@ibm.com>
2019-07-14 12:25:35 -0400

    "v1.7 rank with label and floating resource icons like gold coins go to their respective icons on top of ui"

---
+++ core/src/com/kredatus/flockblockers/GameObjects/Resources/Fuel.java
@@ -0,0 +1,4 @@
+package com.kredatus.flockblockers.GameObjects.Resources;
+
+public class Fuel {
+}
---
+++ core/src/com/kredatus/flockblockers/GameObjects/Resources/Rank.java
@@ -0,0 +1,42 @@
+package com.kredatus.flockblockers.GameObjects.Resources;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.kredatus.flockblockers.Handlers.UiHandler;
+import com.kredatus.flockblockers.Screens.Loader;
+
+public class Rank {
+    public String[] rankNames=new String[]{"Copper","Bronze","Silver","Gold","Diamond","Emerald","Sapphire","Amethyst","Ruby","Pearl","Onyx"};
+    public int[] expValues=new int[858];
+    public int lvl,expGained,remExp;
+
+    public Rank(int lvl, int expGained){
+        if (lvl<857) this.lvl=lvl; else lvl=857;
+
+        expValues[0]=100;
+        for (int i=1;i<=857;i++){
+            expValues[i]*=expValues[i-1]*1.5;
+        }
+        remExp=expValues[lvl]-expGained;
+        this.expGained=expGained;
+    }
+
+    public void addExp(int expToAdd){
+        if (expToAdd<remExp){//if gaining exp without gaining a lvl
+            expGained+=expToAdd;
+            remExp=expValues[lvl]-expGained;
+        } else if (expToAdd-(remExp+expValues[lvl+1])>=0) {//if gaining so much exp that you gain 2 or more levels
+            expToAdd-=remExp+expValues[lvl+1];
+            remExp=expValues[lvl+2]=(expToAdd-(remExp)-expValues[lvl+1]);
+            lvl+=2;
+            addExp(expToAdd);//knew recursivity would come in handy lol
+            UiHandler.rankImage=new Image(Loader.ranksList[lvl]);
+            if (lvl>857) lvl=857;
+        } else {//if gaining a single level
+            expGained=expToAdd-remExp;
+            remExp=expValues[lvl++]-expGained;
+            UiHandler.rankImage=new Image(Loader.ranksList[lvl]);
+            if (lvl>857) lvl=857;
+        }
+
+    }
+}
